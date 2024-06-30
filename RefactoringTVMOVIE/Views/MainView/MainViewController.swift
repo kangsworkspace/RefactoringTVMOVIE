08c93b4556f7f876d1ff546743ac92049ae2b77b
Healthy <kangsworkspace@gmail.com>
2024-06-30 18:59:44 +0900

    feat: add topView
    상단 뷰(텍스트 필드, 버튼 추가)

--- RefactoringTVMOVIE/Views/MainView/MainViewController.swift
+++ RefactoringTVMOVIE/Views/MainView/MainViewController.swift
@@ -18,8 +18,24 @@ class MainViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private let tvTrigger = BehaviorSubject<Int>(value: 1)
     
+    // MARK: - Layouts
+    // 상단 뷰
+    private let topStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 8
+    }
+    
+    private let buttonView = ButtonView()
+    
     private let textField = UITextField().then {
-        $0.text = ""
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.gray.cgColor
+        $0.layer.cornerRadius = 5
+        let imageView = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+        imageView.tintColor = .black
+        imageView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        $0.leftView = imageView
+        $0.leftViewMode = .always
     }
     
     // MARK: - Life Cycles
@@ -28,11 +44,27 @@ class MainViewController: UIViewController {
 
         setUI()
         bindViewModel()
-        tvTrigger.onNext(1)
     }
     
+    // MARK: - Functions
     private func setUI() {
         view.backgroundColor = .white
+        
+        self.view.addSubview(topStackView)
+        topStackView.addArrangedSubview(textField)
+        topStackView.addArrangedSubview(buttonView)
+        
+        topStackView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(12)
+        }
+
+        textField.snp.makeConstraints { make in
+            make.height.equalTo(44)
+        }
+        
+        buttonView.snp.makeConstraints { make in
+            make.height.equalTo(80)
+        }
     }
     
     private func bindViewModel() {
---
+++ RefactoringTVMOVIE/Views/MainView/SubViews/ButtonView.swift
@@ -0,0 +1,53 @@
+//
+//  ButtonView.swift
+//  RefactoringTVMOVIE
+//
+//  Created by Healthy on 6/30/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ButtonView: UIView {
+    // MARK: - Layouts
+    let tvButton = UIButton().then {
+        $0.setTitle("TV", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.configuration = UIButton.Configuration.bordered()
+    }
+    
+    let movieButton = UIButton().then {
+        $0.setTitle("MOVIE", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.configuration = UIButton.Configuration.bordered()
+    }
+    
+    // MARK: - Life Cycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+
+    
+    // MARK: - Funtions
+    private func setUI() {
+        self.addSubview(tvButton)
+        self.addSubview(movieButton)
+        
+        tvButton.snp.makeConstraints { make in
+            make.centerY.leading.equalToSuperview()
+        }
+        
+        movieButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(tvButton.snp.trailing).offset(10)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
